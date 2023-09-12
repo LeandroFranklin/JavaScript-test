d01d83dd936b724d2d8713aa25f655c0ea71f422
Leandro Franklin <leandrofrankss5@gmail.com>
2023-09-12 12:30:59 -0300

    Variáveis

--- meu-script.js
+++ meu-script.js
@@ -1,9 +1,21 @@
-alert('Olá, seja bem-vindo ao curso!')
-document.getElementById('nome').value = 'Oi'
+//String
+var nome = "Leandro";
 
-//comentar linha única
+//Number
+var numInteiro = 22;
 
-/*
-Comentar em várias linhas...
-mesma maneira do CSS
-*/
+//Float / Double
+var numFlutuante = 98.2;
+
+//Boolean
+var boleana = false; //pode ser 'true'
+
+//alert exibe o valor de uma variável no aviso
+alert(nome);
+
+//exibe o valor no browser
+document.write(nome);
+
+//Exibir valor no console do navegador
+console.log(numFlutuante);
+
